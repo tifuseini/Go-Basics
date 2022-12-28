b0b6914cdcfc3ca85ad6ebe7ec524c94fbbe8403
Tahiru Fuseini <tahirufu@stlghana.com>
2022-12-28 18:09:12 +0000

    update to boolean file

--- chapter2/boolean.go
+++ chapter2/boolean.go
@@ -1,5 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(false && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(false || false)
 
 }
