208864b6e0ec8323a44109c8641cdb098c6f9eb9
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-28 19:06:18 +0000

    varible update

--- chapter3/variable.go
+++ chapter3/variable.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+	var x string = "Hello World"
+	var y int = 20
+	a := "Hello "
+	b := "yews"
+	fmt.Println(a == b)
+	fmt.Println(x)
+	fmt.Println(y)
 
 }
