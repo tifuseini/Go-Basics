781f3786009f450b5be40144ec47047b346b9d76
Tahiru Fuseini <tahirufu@stlghana.com>
2022-12-28 18:12:43 +0000

    add not functions

--- chapter2/boolean.go
+++ chapter2/boolean.go
@@ -3,11 +3,18 @@ package main
 import "fmt"
 
 func main() {
+	/** AND */
 	fmt.Println(true && true)
 	fmt.Println(true && false)
 	fmt.Println(false && false)
+
+	/** OR */
 	fmt.Println(true || true)
 	fmt.Println(true || false)
 	fmt.Println(false || false)
+
+	/* NOT */
+	fmt.Println(!true)
+	fmt.Println(!false)
 
 }
