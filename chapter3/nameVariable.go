ce244da96c0a0eec95abbe951a3afee4cc07a7e4
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-28 19:10:42 +0000

    name Variable update

--- chapter3/nameVariable.go
+++ chapter3/nameVariable.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	x := "Max"
+	fmt.Println("My dog's name is ", x)
+
+	name := "Maxine"
+	fmt.Println("My dog's name is ", name)
+
+	dogsName := "Max"
+	fmt.Println("My dog's name is ", dogsName)
 
 }
