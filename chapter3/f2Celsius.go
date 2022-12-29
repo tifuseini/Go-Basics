da405dc0181d84e89ee6b974f67813e64af6d8b4
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-29 06:31:09 +0000

    fahrenheit to celsius update

--- chapter3/f2Celsius.go
+++ chapter3/f2Celsius.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	fmt.Println("Enter Fahrenheit to be converted")
+	var fahrenheit float64
+	fmt.Scanf("%f", &fahrenheit)
+
+	celsius := (fahrenheit - 32) * 5 / 9
+	fmt.Println(celsius)
 
 }
