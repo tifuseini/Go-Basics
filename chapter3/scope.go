449d692d12b7ff2721ff3c2eb202b0361f3a4fcb
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-29 06:17:13 +0000

    scope file update

--- chapter3/scope.go
+++ chapter3/scope.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
+var y string = "Good byw "
+
 func main() {
+	var x string = "Hello world"
+	fmt.Println(x)
+	fmt.Println(y)
+	f()
+
+}
+
+func f() {
+	fmt.Println(y)
 
 }
