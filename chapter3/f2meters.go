ceed0a70e44f336bbc9adb2fb0a2a212399196a1
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-29 06:37:31 +0000

    feet to meter

--- chapter3/f2meters.go
+++ chapter3/f2meters.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	fmt.Println("Enter feet to be converted")
+	var feet float64;
+	fmt.Scanf("%f", &feet)
+
+	meter := feet * 0.3048
+	fmt.Println("Update feet", meter)
 
 }
