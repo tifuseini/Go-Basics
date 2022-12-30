af96b4c30c44e3c6f437995a2ef43327c271808b
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-30 20:51:45 +0000

    new files

--- chapter4/ifStatement.go
+++ chapter4/ifStatement.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	$END$
+
+	for index := 1; index <= 10; index++ {
+		if index%2 == 0 {
+			fmt.Println(index, "even")
+		} else {
+			fmt.Println(index, "odd")
+		}
+
+	}
+
 }
