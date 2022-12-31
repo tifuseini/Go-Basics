4210483f423ac05150b56487ae3d4d830e36f4fc
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-31 19:03:39 +0000

    average test score  update

--- chapter5/averageTestScores.go
+++ chapter5/averageTestScores.go
@@ -1,5 +1,24 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	var testScores [5]float64
+	testScores[0] = 98
+	testScores[1] = 98
+	testScores[2] = 90
+	testScores[3] = 78
+	testScores[4] = 9
+
+	var totalTestScores float64 = 0
+	for index := 0; index < 5; index++ {
+		totalTestScores += testScores[index]
+
+	}
+
+	average := totalTestScores / 5
+
+	fmt.Println(average)
 
 }
