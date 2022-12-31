ef24daf9b0ed3e9009ba8a1f265ecc878befbcfa
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-31 20:05:13 +0000

    average function

--- chapter6/function.go
+++ chapter6/function.go
@@ -1,5 +1,23 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	xs := []float64{90, 89, 34, 34, 45}
+	var averaged = average(xs)
+	fmt.Println(averaged)
+
+}
+
+func average(xs []float64) float64 {
+
+	total := 0.0
+
+	for _, value := range xs {
+		total += value
+	}
+
+	return total / float64(len(xs))
 
 }
