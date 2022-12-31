e0e61b81e5f33e4d110e9629a15a6931d39e67c7
tifuseini <tifuseini@st.ug.edu.gh>
2022-12-31 18:44:00 +0000

    switch file update

--- chapter4/switchStatement.go
+++ chapter4/switchStatement.go
@@ -1,5 +1,19 @@
 package main
 
+import "fmt"
+
 func main() {
+
+	for index := 1; index <= 10; index++ {
+
+		switch index {
+		case 1:
+			fmt.Println("one")
+		default:
+			fmt.Println("Unknown Number")
+
+		}
+
+	}
 
 }
