efbc462f7048faf2752d84a7523170a54b1d0160
tifuseini <tifuseini@st.ug.edu.gh>
2023-01-05 20:23:31 +0000

    embedded type

--- chapter7/embeddedType.go
+++ chapter7/embeddedType.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
+type Person struct {
+	name string
+	age  string
+}
+
+func (p *Person) Talk() {
+	fmt.Println("Hi my name is ", p.name)
+	fmt.Println("I am  ", p.age)
+}
+
+type Android struct {
+	Person Person
+	Model  string
+}
+
 func main() {
+
+	a := new(Android)
+	a.Person.Talk()
 
 }
